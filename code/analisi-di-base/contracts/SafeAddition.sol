3e451702aebcdadeb6f31c5787661b7227343620
Samuele Ferri <samuelexferri@gmail.com>
2021-03-15 17:16:54 +0100

    Add SafeAddition (SafeMath)

---
+++ code/analisi-di-base/contracts/SafeAddition.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+import './SafeMath.sol';
+ 
+contract SafeAddition {
+    
+    function safeAdd(uint256 a, uint256 b) public pure returns(uint256) {
+        return SafeMath.add(a, b);
+    }
+  
+    function safeAddOwn(uint256 a, uint256 b) public pure returns(uint256) {
+        uint c = a + b;
+        require(c >= a, "Addition overflow");
+        return c;
+    }
+}
