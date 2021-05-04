34bc6c9cac4ceec1e5fc45b834d4ad218eedd818
Samuele Ferri <samuelexferri@gmail.com>
2021-05-04 10:29:09 +0200

    Add Dracula UI

--- react/notarizeth/src/App.js
+++ react/notarizeth/src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 
+import '@dracula/dracula-ui/styles/dracula-ui.css'
+import { Paragraph } from '@dracula/dracula-ui'
+
 function App() {
   return (
     <div className="App">
@@ -17,6 +20,7 @@ function App() {
         >
           Learn React
         </a>
+		<Paragraph color="black">Hello Vampire</Paragraph>
       </header>
     </div>
   );
