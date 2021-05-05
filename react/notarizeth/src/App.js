704778928971bce7cf3f1e4f16505c1a4ee0aba4
Samuele Ferri <samuelexferri@gmail.com>
2021-05-05 11:46:05 +0200

    Update App.js

--- react/notarizeth/src/App.js
+++ react/notarizeth/src/App.js
@@ -1,10 +1,17 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 import '@dracula/dracula-ui/styles/dracula-ui.css'
 import { Button, Paragraph } from '@dracula/dracula-ui'
 
+import { useWeb3 } from '@openzeppelin/network/react';
+const infuraProjectId = '773217385b694a6ea60c4cea1a430a09';
+
 function App() {
+  const web3Context = useWeb3(`wss://mainnet.infura.io/ws/v3/${infuraProjectId}`);
+  const { networkId, networkName, providerName } = web3Context;
+
   return (
     <div className="App">
       <header className="App-header">
@@ -23,6 +30,15 @@ function App() {
 		    <Paragraph color="black">Hello Vampire</Paragraph>
         <Button variant='ghost'>Button</Button>
       </header>
+      <div>
+        <h1>Infura/MetaMask/OpenZeppelin Dapp</h1>
+        <div>
+          Network: {networkId ? `${networkId} – ${networkName}` : 'No connection'}
+        </div>
+        <div>
+        Provider: {providerName}
+        </div>
+      </div>
     </div>
   );
 }
