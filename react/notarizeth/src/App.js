3c0e6f97c820267abf7a40ea8720dd9a5b96a945
Samuele Ferri <samuelexferri@gmail.com>
2021-05-05 12:01:01 +0200

    Update App.js

--- react/notarizeth/src/App.js
+++ react/notarizeth/src/App.js
@@ -1,17 +1,55 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+import React from 'react'
+import { Web3ReactProvider } from '@web3-react/core'
+import { Web3Provider } from '@ethersproject/providers'
+import { useWeb3React } from '@web3-react/core'
+import { InjectedConnector } from '@web3-react/injected-connector'
 
 import '@dracula/dracula-ui/styles/dracula-ui.css'
 import { Button, Paragraph } from '@dracula/dracula-ui'
 
-import { useWeb3 } from '@openzeppelin/network/react';
-const infuraProjectId = '773217385b694a6ea60c4cea1a430a09';
+
+export const injectedConnector = new InjectedConnector({
+  supportedChainIds: [
+    //1, // Mainet
+    3, // Ropsten
+    //4, // Rinkeby
+    //5, // Goerli
+    //42, // Kovan
+  ],
+})
+
+function getLibrary(provider: any): Web3Provider {
+  const library = new Web3Provider(provider)
+  library.pollingInterval = 12000
+  return library
+}
+
+export const Wallet = () => {
+  const { chainId, account, activate, active } = useWeb3React<Web3Provider>()
+
+  const onClick = () => {
+    activate(injectedConnector)
+  }
+
+  return (
+    <div>
+      <div>ChainId: {chainId}</div>
+      <div>Account: {account}</div>
+      {active ? (
+        <div>✅ </div>
+      ) : (
+        <button type="button" onClick={onClick}>
+          Connect
+        </button>
+      )}
+    </div>
+  )
+}
 
 function App() {
-  const web3Context = useWeb3(`wss://mainnet.infura.io/ws/v3/${infuraProjectId}`);
-  const { networkId, networkName, providerName } = web3Context;
-
   return (
     <div className="App">
       <header className="App-header">
@@ -30,15 +68,10 @@ function App() {
 		    <Paragraph color="black">Hello Vampire</Paragraph>
         <Button variant='ghost'>Button</Button>
       </header>
-      <div>
-        <h1>Infura/MetaMask/OpenZeppelin Dapp</h1>
-        <div>
-          Network: {networkId ? `${networkId} – ${networkName}` : 'No connection'}
-        </div>
-        <div>
-        Provider: {providerName}
-        </div>
-      </div>
+
+      <Web3ReactProvider getLibrary={getLibrary}>
+        <Wallet />
+      </Web3ReactProvider>
     </div>
   );
 }
